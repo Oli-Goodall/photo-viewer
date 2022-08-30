3e85f5e8590f2e319a37c428ca00ce5dd0c4b310
Konstantinos <kon.xen@:gmail.com>
2022-08-30 14:59:32 +0100

    refactoring image selector to gallery

--- src/Components/Gallery.tsx
+++ src/Components/Gallery.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";  // import React (to provide access to JSX)
 import { imageUrls } from './GetURLs'
+import './gallery.css'
 
-export function ImageSelector() {
+
+export function Gallery() {
 
     const [photoUrl, setPhotoUrl] = useState('');
 
     // when image is clicked { set state to selected image URL}
-
+    
     return (
         <div className="imageSelector">
             {
